7699ea488766577165fc218295608cfdfc75a27a
Marius Dalseng <mfloresdal@gmail.com>
2026-07-20 23:04:44 +0200

    copied in test rom

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod chip8;
-mod memory;
 mod cpu;
+mod memory;
 use chip8::Chip8;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut chip8 = Chip8::new();
-    chip8.load_rom("roms/some_rom.ch8")?;
+    chip8.load_rom("./roms/ibm-logo.ch8")?;
     // print/inspect memory here to verify it loaded
     Ok(())
 }
