a5ce0e43f49cc715d23faf54b57c6533cab63b76
Marius Dalseng <mfloresdal@gmail.com>
2026-07-20 23:00:27 +0200

    load_rom implemented, returns error due to rom not existing currently

--- src/memory.rs
+++ src/memory.rs
@@ -5,13 +5,16 @@ pub struct Memory {
 impl Memory {
     pub fn new() -> Self {
         // initialize ram to all zeros, load font set at 0x50
-        Memory { ram: [0 ; 4096] }
+
+        Memory { ram: [0; 4096] }
     }
 
-    pub fn load_rom(&mut self, path: &str) -> Result<(), Box<dyn std::error::Error>>{
+    pub fn load_rom(&mut self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
         // this is where std::fs::read(path)? and copy_from_slice go
 
-        println!("{}", path);
+        let rom_bytes: Vec<u8> = std::fs::read(path)?; // read raw bytes from file
+        self.ram[0x200..0x200 + rom_bytes.len()].copy_from_slice(&rom_bytes);
+
         Ok(())
     }
 }
